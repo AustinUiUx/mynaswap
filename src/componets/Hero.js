488df60ed7fb43c0ca20c403693e7c48e349f3ec
Austin Edmondson <austinui@Austins-MBP.lan>
2022-12-05 16:25:10 -0800

    smallerButton

---
+++ src/componets/Hero.js
@@ -0,0 +1,16 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+const Hero = () => {
+    return(
+
+        <Container>
+            <Typography>
+                Testing From Hero.js
+            </Typography>
+        </Container>
+
+    )
+}
+
+export default Hero
